1f943a4408cd0395bb5811e9aa3ae243b5191273
fly9593 <59961051+fly9593@users.noreply.github.com>
2024-01-04 09:58:10 +0800

    适配自动下一页插件

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         SteamWallpaperDeblur
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  去除Wallpaper创意工坊中R18壁纸的缩略图模糊效果(Removes blur effect from adult content images in Steam Wallpaper workshop)
+// @version      0.5
+// @description  Removes blur effect from adult content images in Steam Wallpaper workshop
 // @author       fly9593
 // @match        https://steamcommunity.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
@@ -21,6 +21,20 @@
         });
     }
 
-    // Call the removeBlur function
+    // Call the removeBlur function initially
     removeBlur();
+
+    // Use MutationObserver to watch for changes in the DOM
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                // If new content is added, call removeBlur function
+                removeBlur();
+            }
+        });
+    });
+
+    // Configure and start the observer
+    var observerConfig = { childList: true, subtree: true };
+    observer.observe(document.body, observerConfig);
 })();
